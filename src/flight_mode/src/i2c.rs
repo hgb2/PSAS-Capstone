7f410cedd5387a9cd15added714275763625d281
brianbreniser <brianbreniser@gmail.com>
2016-06-26 15:23:14 -0700

    fixed i2c problems

--- src/flight_mode/src/i2c.rs
+++ src/flight_mode/src/i2c.rs
@@ -1,7 +1,6 @@
 use i2cdev::linux::{LinuxI2CDevice, LinuxI2CError};
 
 pub fn init(path: &'static str, slave_address: u16) -> Result<LinuxI2CDevice, i32> {
-    //i2cdev::linux::LinuxI2CDevice::new(path, slave_address)
     let mut x = LinuxI2CDevice::new(path, slave_address);
     match x {
         Ok(y) => return Ok(y),
