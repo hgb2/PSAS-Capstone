0d30316f56916b48bf579961b85cf33b2059bc24
Tyler Alway <Tyler@Tylers-MacBook-Pro.local>
2016-06-28 22:12:12 -0700

    Copied from Brians repository for testing.

--- src/flight_mode/src/i2c.rs
+++ src/flight_mode/src/i2c.rs
@@ -1,8 +1,10 @@
-pub fn init() {
-    println!("flight mode i2c init");
-}
 
-pub fn update(x: i32) -> i32 {
-    println!("flight mode i2c update received {}", x);
-    x
+use i2cdev::linux::{LinuxI2CDevice, LinuxI2CError};
+
+pub fn init(path: &'static str, slave_address: u16) -> Result<LinuxI2CDevice, i32> {
+    let mut x = LinuxI2CDevice::new(path, slave_address);
+    match x {
+        Ok(y) => return Ok(y),
+        Err(_) => return Err(1),
+    }
 }
