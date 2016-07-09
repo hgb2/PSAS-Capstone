0329507a01a4c7ecbff1f1b6fd025cc7c554980f
Chris Liebert <liebert@pdx.edu>
2016-07-09 15:46:04 -0700

    Adapted new GPIO interface and added a CMake build script that links libwrapper.so to JSBSim.

---
+++ src/test_mode/src/JSBSimWrapper.cpp
@@ -0,0 +1,9 @@
+#include "JSBSimWrapper.h"
+
+void initJSBSim()
+{ 
+	JSBSim::FGFDMExec FDMExec;
+	printf("Started JSBSim\n");
+	//FDMExec.LoadScript("run.xml");
+	//FDMExec.Run();
+}
---
+++ src/test_mode/src/JSBSimWrapper.h
@@ -0,0 +1,9 @@
+#ifndef _JSBSIM_WRAPPER_H_
+#define _JSBSIM_WRAPPER_H_
+
+#include <FGFDMExec.h>
+#include <stdio.h>
+
+void initJSBSim();
+
+#endif
