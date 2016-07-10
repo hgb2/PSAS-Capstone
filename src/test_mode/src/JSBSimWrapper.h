209d3c9c546f6314a66f1e4a9a0176af3d1282dd
sanayani <sanayani@users.noreply.github.com>
2016-07-09 19:40:21 -0700

    update

--- src/test_mode/src/JSBSimWrapper.h
+++ src/test_mode/src/JSBSimWrapper.h
@@ -5,9 +5,12 @@
 #include <iostream>
 
 extern "C" {
-	//void wrapper_init();
-	//void wrapper_update();
-	//void wrapper_terminate();
+	//placeholder functions
+	void wrapper_init();
+	void wrapper_update();
+	void wrapper_terminate();
+	
+	//rust api
 	JSBSim::FGFDMExec* fdm_create();
 	void fdm_get_version(JSBSim::FGFDMExec* fdm);
 	void fdm_close(JSBSim::FGFDMExec* fdm);
