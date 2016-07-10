2118fe65b79771d581d973278333fb6a58067bf1
Tyler Alway <tyler.alway@gmail.com>
2016-07-09 21:12:29 -0700

    Moved the path to this file for the i2c device, check to see if this is the correct device, init the registers that we will be using.

--- src/flight_mode/src/i2c.rs
+++ src/flight_mode/src/i2c.rs
@@ -1,10 +1,39 @@
 
-use i2cdev::linux::{LinuxI2CDevice, LinuxI2CError};
+//use i2cdev::linux::{LinuxI2CDevice, LinuxI2CError};
+use i2cdev::core::*;
+use i2cdev::linux::*;
 
-pub fn init(path: &'static str, slave_address: u16) -> Result<LinuxI2CDevice, i32> {
-    let mut x = LinuxI2CDevice::new(path, slave_address);
+fn read_reg(bus: &mut LinuxI2CDevice, reg: u8, buf: &mut [u8]) {
+    bus.write(&[reg]); // 0x43 is the beginning address of the block of registers that we want to read
+    bus.read(buf); // puts block (buf.length) of registers in buf (accel, temp, and gyro.
+}
+
+
+pub fn init() -> Result<LinuxI2CDevice, String> {
+    let mut x = LinuxI2CDevice::new("/dev/i2c-1", 0x68);
+    let mut dev;
     match x {
-        Ok(y) => return Ok(y),
-        Err(_) => return Err(1),
+        Ok(y) => dev = y,
+        Err(e) => return Err(format!("Failed to open the i2c device")),
     }
+
+
+
+    let mut buf = [0u8; 1];
+	read_reg(&mut dev, 0x75, &mut buf);
+	if buf[0] != 0x68 {
+		return Err(format!("MPU-9150 WhoAmI returned wrong value"));
+	}
+
+    // Wake device up, using internal oscillator.
+    dev.write(&[0x6b, 0x00]);
+
+    // Set configuration:
+    // - Sample rate divider: 1kHz / 200
+    // - Config: no FSYNC, low-pass filter at 5Hz
+    // - Gyro config: full scale range at +/- 250 dps
+    // - Accel config: full scale range at +/- 2g
+    dev.write(&[0x19, 199, 0x06, 0x00, 0x00]);
+
+    return Ok(dev);
 }
