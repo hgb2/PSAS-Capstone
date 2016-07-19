4cc0fc2862b084b9d01172f9ecb926f168dc145c
sanayani <sanayani@users.noreply.github.com>
2016-07-18 17:21:22 -0700

    update binder.rs with placeholder functions
    test mode should call these functions (rather than call wrapper functions directly).  s.

--- src/test_mode/src/binder.rs
+++ src/test_mode/src/binder.rs
@@ -1,3 +1,23 @@
 //binder.rs
 //
 //
+extern crate libc;
+//mod wrapper;
+
+//placeholder functions
+pub fn binder_init()->*mut wrapper::FDM{
+    println!("binder init");
+    
+    unsafe{
+        let fdm = wrapper::fdm_create();
+        return fdm;
+    }    
+}
+
+pub fn binder_step(fdm: *mut wrapper::FDM ){
+    println!("binder step");
+}
+
+pub fn binder_close(fdm: *mut wrapper::FDM ){
+    println!("binder close");
+}
