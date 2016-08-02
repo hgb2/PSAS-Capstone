cd4d6b3e36029a8ef2e567d4179fb1184b2fafc1
sanayani <sanayani@users.noreply.github.com>
2016-08-02 06:31:09 -0700

    clean up tabs

--- src/test_mode/src/wrapper.cpp
+++ src/test_mode/src/wrapper.cpp
@@ -10,7 +10,7 @@ JSBSim::FGFDMExec* fdm_create(){
 
 //destructor
 void fdm_close(JSBSim::FGFDMExec *fdm){
-	//coordinate with both rust & jsbsim
+    //coordinate with both rust & jsbsim
     //delete fdm;
 }
 
@@ -20,7 +20,7 @@ bool fdm_run(JSBSim::FGFDMExec *fdm){
 }
 
 bool fdm_run_ic(JSBSim::FGFDMExec *fdm){
-	return fdm->RunIC();
+    return fdm->RunIC();
 }
 
 bool fdm_load_script(JSBSim::FGFDMExec *fdm, const char* script_name, double delta_t, const char* init_file){
@@ -31,21 +31,21 @@ bool fdm_load_script(JSBSim::FGFDMExec *fdm, const char* script_name, double del
 
 bool fdm_set_engine_path(JSBSim::FGFDMExec *fdm, const char* engine_path){
     std::string engine_path_cpp = engine_path;
-	return fdm->SetEnginePath(engine_path_cpp);
+    return fdm->SetEnginePath(engine_path_cpp);
 }
 
 bool fdm_set_aircraft_path(JSBSim::FGFDMExec *fdm, const char* aircraft_path){
     std::string aircraft_path_cpp = aircraft_path;
-	return fdm->SetAircraftPath(aircraft_path_cpp);
+    return fdm->SetAircraftPath(aircraft_path_cpp);
 }
 
 bool fdm_set_systems_path(JSBSim::FGFDMExec *fdm, const char* systems_path){
     std::string systems_path_cpp = systems_path;
-	return fdm->SetSystemsPath(systems_path_cpp);
+    return fdm->SetSystemsPath(systems_path_cpp);
 }
 
 void fdm_set_root_dir(JSBSim::FGFDMExec *fdm, const char* root_dir){
     std::string root_dir_cpp = root_dir;
-	fdm->SetRootDir(root_dir_cpp);
+    fdm->SetRootDir(root_dir_cpp);
 }
 
